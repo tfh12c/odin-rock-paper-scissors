6a0991d679c72cf926e65c0d3964f12cd62439ff
Travis Hunsberger <tfhunsberger@gmail.com>
2022-06-29 13:36:35 -0400

    Refactored computerPlay function. playGame logic created.

--- index-page.js
+++ index-page.js
@@ -1,8 +1,41 @@
-const computerChoice = ["Rock", "Paper", "Scissors"];
-
-function computerPlay(computerChoice) {
-    console.log(computerChoice[Math.floor(Math.random() * computerChoice.length)]);
-    return computerChoice[Math.floor(Math.random() * computerChoice.length)];
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    
+    switch (randomNumber) {
+        case 0: 
+            return "Rock"
+        case 1:
+            return "Paper"
+        case 2: 
+            return "Scissors"
+    }
 }
 
-computerPlay(computerChoice);
+console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("You tied.");
+        return "You tied."
+    }
+    if ( //Player Wins Logic
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper") ||
+        (playerSelection === "Paper" && computerSelection === "Rock")
+    ) {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}!`);
+        return `You win! ${playerSelection} beats ${computerSelection}!`
+    }
+    if ( //Computer Wins Logic
+        (computerSelection === "Rock" && playerSelection === "Scissors") ||
+        (computerSelection === "Scissors" && playerSelection === "Paper") ||
+        (computerSelection === "Paper" && playerSelection === "Rock")
+    ) {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const playerSelection = "Paper";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
